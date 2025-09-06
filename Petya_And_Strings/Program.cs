8105c3afde20af100bef39849718238ca80c34ae
tuananh <anhntlinh@gmail.com>
2025-09-06 12:06:58 +0700

    Complete Petya And Strings

---
+++ Petya_And_Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Petya_And_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string let1 = Console.ReadLine();
+            string let2 = Console.ReadLine();
+            let1 = let1.ToLower();
+            let2 = let2.ToLower();
+            int res = 0;
+            for (int i = 0; i < let1.Length; i++)
+            {
+                if (let1[i] < let2[i])
+                {
+                    res = -1;
+                    break;
+                }
+                else if (let1[i] > let2[i])
+                {
+                    res = 1;
+                    break;
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
