7b627bb7f7ef8e27f2fc0419f6540c814259658b
tuananh <anhntlinh@gmail.com>
2025-09-08 19:26:35 +0700

    complete

---
+++ Bear_And_Big_Brother/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bear_And_Big_Brother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] wei = input.Split(' ');
+            int l = Int32.Parse(wei[0]);
+            int b = Int32.Parse(wei[1]);
+            int i = 0;
+            while(l <= b)
+            {
+                l *= 3;
+                b *= 2;
+                i++;
+            }
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
