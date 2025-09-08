3aeba1933522bd32824300f061d53143d68086d7
tuananh <anhntlinh@gmail.com>
2025-09-08 09:58:15 +0700

    Complete !

--- Helpful_Maths/Program.cs
+++ Helpful_Maths/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Helpful_Maths
 {
@@ -10,6 +6,43 @@ namespace Helpful_Maths
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            string[] numbers = s.Split('+');
+            int len = numbers.Length;
+            BubbleSort(numbers, len);
+            for (int i = 0; i < len; i++)
+            {
+                if (i == len - 1)
+                {
+                    Console.Write(numbers[i]);
+                    break;
+                }
+                Console.Write(numbers[i] + '+');
+
+            }
+        }
+
+        static void BubbleSort(string[] arr, int n)
+        {
+            bool swapped;
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    int a1 = Int32.Parse(arr[j]);
+                    int a2 = Int32.Parse(arr[j + 1]);
+                    if (a1 <= a2) continue;
+
+                    string temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+
+                if (swapped == false)
+                    break;
+            }
         }
     }
 }
