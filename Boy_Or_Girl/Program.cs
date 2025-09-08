dd5c17c982bb61bf41b7825736e9ee9fbdd06d9f
tuananh <anhntlinh@gmail.com>
2025-09-08 10:33:53 +0700

    Complete !

--- Boy_Or_Girl/Program.cs
+++ Boy_Or_Girl/Program.cs
@@ -6,6 +6,34 @@ namespace Boy_Or_Girl
     {
         static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            int len = name.Length;
+            bool isHave;
+            string s = "";
+            for (int i = 0; i < len; ++i)
+            {
+                isHave = false;
+                for (int j = 0; j < s.Length; ++j)
+                {
+                    if (name[i] == s[j])
+                    {
+                        isHave = true;
+                        break;
+                    }
+                }
+                
+                if (isHave == false)
+                {
+                    s += name[i];
+                }
+            }
+
+            if (s.Length % 2 == 0)
+                Console.WriteLine("CHAT WITH HER!");
+            else
+                Console.WriteLine("IGNORE HIM!");
+
+            Console.ReadLine();
         }
     }
 }
