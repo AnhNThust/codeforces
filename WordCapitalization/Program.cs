27d7e0e2fe31612cb44dda41e452ea38097fad35
tuananh <anhntlinh@gmail.com>
2025-09-08 19:01:07 +0700

    Update

--- WordCapitalization/Program.cs
+++ WordCapitalization/Program.cs
@@ -6,6 +6,17 @@ namespace WordCapitalization
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            //if (s[0] >= 'A' && s[0] <= 'Z')
+            if (Char.IsUpper(s[0]))
+                Console.WriteLine(s);
+            else
+            {
+                s[0] = (char)(s[0] - 32);
+                // Console.WriteLine($"{(char)(s[0]-32)}{s.Substring(1)}");
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
         }
     }
 }
