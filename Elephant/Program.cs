a6b2d4e316526b2ddb03e3d163f5dcc8e17bbb4c
tuananh <anhntlinh@gmail.com>
2025-09-09 10:31:55 +0700

    complete

--- Elephant/Program.cs
+++ Elephant/Program.cs
@@ -6,6 +6,17 @@ namespace Elephant
     {
         static void Main(string[] args)
         {
+            int x = Int32.Parse(Console.ReadLine());
+            int step = Step(x);
+            Console.WriteLine(step);
+            Console.ReadLine();
+        }
+
+        static int Step(int len)
+        {
+            return len <= 5 ? 1 : 
+                len % 5 == 0 ? len / 5 :
+                (len / 5) + 1;
         }
     }
 }
