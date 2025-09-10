727ef0cf50c5489c6f9807fad8fe26ef70349b75
tuananh <anhntlinh@gmail.com>
2025-09-10 12:43:00 +0700

    complete

--- Nearly_Lucky_Number/Program.cs
+++ Nearly_Lucky_Number/Program.cs
@@ -6,6 +6,21 @@ namespace Nearly_Lucky_Number
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+            int count = 0;
+            int len = input.Length;
+            for (int i = 0; i < len; ++i)
+            {
+                if (input[i] != '4' && input[i] != '7') continue;
+                else if (count == 0 && i == len - 3) break;
+                count++;
+            }
+
+            if (count != 4 && count != 7)
+                Console.WriteLine("NO");
+            else
+                Console.WriteLine("YES");
+            Console.ReadLine();
         }
     }
 }
