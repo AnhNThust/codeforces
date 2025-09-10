58386a6cab47e24233d002758e6f502e4133e0cf
tuananh <anhntlinh@gmail.com>
2025-09-10 12:22:28 +0700

    complete

--- Wrong_Subtraction/Program.cs
+++ Wrong_Subtraction/Program.cs
@@ -6,6 +6,20 @@ namespace Wrong_Subtraction
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            int n = Int32.Parse(numbers[0]);
+            int k = Int32.Parse(numbers[1]);
+
+            for (int i = 0; i < k; ++i)
+            {
+                if (n % 10 == 0)
+                    n /= 10;
+                else
+                    n--;
+            }
+            Console.WriteLine(n);
+            Console.ReadLine();
         }
     }
 }
