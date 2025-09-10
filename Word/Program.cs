1968cbae67c1b9cc6c24492b36a2a98843e1988f
tuananh <anhntlinh@gmail.com>
2025-09-10 10:28:58 +0700

    complete

--- Word/Program.cs
+++ Word/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Word
 {
@@ -6,6 +7,22 @@ namespace Word
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            int len = s.Length;
+            int count = 0;
+            //List<int> indexes = new List<int>(); // Luu cac tu la chu cai thuong
+            for (int i = 0; i < len; ++i)
+            {
+                if (char.IsUpper(s[i])) continue;
+                count++;
+                //indexes.Add(i);
+            }
+
+            if (count >= (float)len / 2)
+                Console.WriteLine(s.ToLower());
+            else
+                Console.WriteLine(s.ToUpper());
+            Console.ReadLine();
         }
     }
 }
