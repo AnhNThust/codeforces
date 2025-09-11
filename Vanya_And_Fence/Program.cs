93a6882814c5538dc24605c44fbe15277a80975f
tuananh <anhntlinh@gmail.com>
2025-09-11 17:13:00 +0700

    complete

--- Vanya_And_Fence/Program.cs
+++ Vanya_And_Fence/Program.cs
@@ -6,6 +6,19 @@ namespace Vanya_And_Fence
     {
         static void Main(string[] args)
         {
+            string[] input1 = Console.ReadLine().Split(' '); // n & h
+            string[] input2 = Console.ReadLine().Split(' '); // n (ai)
+            int n = Int32.Parse(input1[0]);
+            int h = Int32.Parse(input1[1]);
+            int res = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                int hei = Int32.Parse(input2[i]);
+                if (hei <= h) res += 1;
+                else res += 2;
+            }
+            Console.WriteLine(res);
+            Console.ReadLine();
         }
     }
 }
