39f70b61eca03d2bbbd6d661f109fc086f80412f
tuananh <anhntlinh@gmail.com>
2025-09-11 09:39:00 +0700

    not complete

--- Anton_And_Danik/Program.cs
+++ Anton_And_Danik/Program.cs
@@ -6,7 +6,23 @@ namespace Anton_And_Danik
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
 
+            int countA = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                if (s[i] == 'D') continue;
+                countA++;
+            }
+
+            if (countA > (float)n / 2)
+                Console.WriteLine("Anton");
+            else if (countA < (float)n / 2)
+                Console.WriteLine("Danik");
+            else
+                Console.WriteLine("Friendship");
+            Console.ReadLine();
         }
     }
 }
