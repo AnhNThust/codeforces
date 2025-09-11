601f3183af930f19e5a2d8ab196a42fdcbca4b6f
tuananh <anhntlinh@gmail.com>
2025-09-11 10:35:04 +0700

    complete

--- Translation/Program.cs
+++ Translation/Program.cs
@@ -5,6 +5,28 @@ namespace Translation
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+
+            string result = CheckReverse(s, t);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static string CheckReverse(string s, string t)
+        {
+            int lenS = s.Length;
+            int lenT = t.Length;
+
+            if (lenS != lenT) return "NO";
+            
+            for (int i = 0; i < lenS; ++i)
+            {
+                if (s[i] == t[lenS - i - 1]) continue;
+                return "NO";
+            }
+
+            return "YES";
         }
     }
 }
