a77eddf6fe8ff9fc7a7ecb48a593265273372adf
tuananh <anhntlinh@gmail.com>
2025-09-13 01:28:18 +0700

    complete

--- In_Search_Of_An_Easy_Problem/Program.cs
+++ In_Search_Of_An_Easy_Problem/Program.cs
@@ -6,6 +6,15 @@ namespace In_Search_Of_An_Easy_Problem
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            string nums = Console.ReadLine();
+            Console.WriteLine(CheckProblem(nums));
+            Console.ReadLine();
+        }
+
+        static string CheckProblem(string input)
+        {
+            return input.IndexOf("1") >= 0 ? "HARD" : "EASY"; 
         }
     }
 }
