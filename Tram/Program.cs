68c72c3881bc843028628d4b0b5e04ed483662d5
tuananh <anhntlinh@gmail.com>
2025-09-12 11:09:49 +0700

    complete

--- Tram/Program.cs
+++ Tram/Program.cs
@@ -6,6 +6,22 @@ namespace Tram
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            int max = 0;
+            int now = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int off = Int32.Parse(input[0]);
+                int on = Int32.Parse(input[1]);
+                now = now - off + on;
+
+                if (now <= max) continue;
+                max = now;
+            }
+
+            Console.WriteLine(max);
+            Console.ReadLine();
         }
     }
 }
