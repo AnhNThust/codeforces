90249a6526f98bd0685360d752eebc3fc9256676
tuananh <anhntlinh@gmail.com>
2025-09-14 16:38:37 +0700

    complete

--- George_and_Accommodation/Program.cs
+++ George_and_Accommodation/Program.cs
@@ -6,6 +6,22 @@ namespace George_and_Accommodation
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            string[] input;
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split(' ');
+                int p = Int32.Parse(input[0]);
+                int q = Int32.Parse(input[1]);
+
+                //if (q - p < 2) continue;
+                //count++;
+                if (q - p >= 2) continue;
+                count++;
+            }
+            Console.WriteLine(n - count);
+            Console.ReadLine();
         }
     }
 }
