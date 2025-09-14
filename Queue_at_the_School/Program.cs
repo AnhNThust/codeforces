4f9b2beda78ce53de278cf3e9bd5890a8714cc56
tuananh <anhntlinh@gmail.com>
2025-09-14 12:08:03 +0700

    complete

--- Queue_at_the_School/Program.cs
+++ Queue_at_the_School/Program.cs
@@ -7,42 +7,50 @@ namespace Queue_at_the_School
         static void Main(string[] args)
         {
             string[] input = Console.ReadLine().Split(' ');
-            //string s = Console.ReadLine();
             char[] ch = Console.ReadLine().ToCharArray();
             int n = Int32.Parse(input[0]);
             int t = Int32.Parse(input[1]);
-            string res = "";
-            int count;
-            for (int i = 0; i < n; i += count + 1)
+            int head = 0;
+            int tail = n - 1;
+            for (int i = 0; i < t; i++)
             {
-                count = 0;
-                if (ch[i] == 'G')
-                {
-                    res += ch[i];
-                    continue;
-                }
+                if (ch[head] == 'G')
+                    head++;
 
-                t += i;
-                for (int j = i; j < t; ++j)
+                if (ch[tail] == 'B')
+                    tail--;
+
+                if (head >= tail) break;
+
+                for (int j = head; j < tail; j++)
                 {
-                    //GBGBG t = 2
-                    if (ch[j] == 'B' && ch[j + 1] == 'G')
+                    //if (ch[j] == 'B')
+                    //{
+                    //    if (ch[j + 1] == 'G')
+                    //    {
+                    //        char temp = ch[j];
+                    //        ch[j] = ch[j + 1];
+                    //        ch[j + 1] = temp;
+                    //        j++;
+                    //    }
+                    //}
+                    //else continue;
+
+                    // Cach viet duoi tuong tu nhu cach tren
+                    if (ch[j] == 'G') continue;
+                        
+                    if (ch[j + 1] == 'G')
                     {
                         char temp = ch[j];
                         ch[j] = ch[j + 1];
                         ch[j + 1] = temp;
-                        count++;
-                        //if (ch[j + 1] == 'G')
-                        //{
-                        //    res += s[j + 1];
-                        //    res += s[j];
-                        //    count++;  
-                        //}
+                        j++;
                     }
                 }
             }
 
-            Console.WriteLine(res);
+            for (int i = 0; i < n; i++)
+                Console.Write(ch[i]);
             Console.ReadLine();
         }
     }
