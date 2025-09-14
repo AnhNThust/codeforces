73bd09b4c6057aedf047913cf0f2f856a0a5fe1f
tuananh <anhntlinh@gmail.com>
2025-09-14 22:40:13 +0700

    complete

--- Calculating_Function/Program.cs
+++ Calculating_Function/Program.cs
@@ -6,6 +6,14 @@ namespace Calculating_Function
     {
         static void Main(string[] args)
         {
+            long n = long.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+                Console.WriteLine(n / 2);
+            else
+                Console.WriteLine((-1 - n) / 2);
+            
+            Console.ReadLine();
         }
     }
 }
