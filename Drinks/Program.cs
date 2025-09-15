195cb54e6d0b00fadea6b43a6d4d932c0d865d05
tuananh <anhntlinh@gmail.com>
2025-09-15 10:52:17 +0700

    complete

--- Drinks/Program.cs
+++ Drinks/Program.cs
@@ -5,6 +5,18 @@ namespace Drinks
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (input[i] == "0") continue;
+                int perI = Int32.Parse(input[i]);
+                sum += perI;
+            }
+
+            Console.WriteLine(sum / n);
+            Console.ReadLine();
         }
     }
 }
