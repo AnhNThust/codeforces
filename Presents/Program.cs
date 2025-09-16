e67264faf2df7bdb2e77d5b40a7989728e7b9e4f
tuananh <anhntlinh@gmail.com>
2025-09-16 13:55:50 +0700

    complete

--- Presents/Program.cs
+++ Presents/Program.cs
@@ -6,6 +6,22 @@ namespace Presents
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            string[] p = Console.ReadLine().Split(' ');
+            string[] q = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < p.Length; j++)
+                {
+                    if ((i + 1) != Int32.Parse(p[j])) continue;
+                    q[i] = $"{j + 1}";
+                    break;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+                Console.Write($"{q[i]} ");
+            Console.ReadLine();
         }
     }
 }
