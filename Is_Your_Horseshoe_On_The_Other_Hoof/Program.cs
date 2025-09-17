6a53038b5f82f90b5f088ddd03e96c6a2217811e
tuananh <anhntlinh@gmail.com>
2025-09-17 12:10:29 +0700

    complete

--- Is_Your_Horseshoe_On_The_Other_Hoof/Program.cs
+++ Is_Your_Horseshoe_On_The_Other_Hoof/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Is_Your_Horseshoe_On_The_Other_Hoof
 {
@@ -6,6 +7,15 @@ namespace Is_Your_Horseshoe_On_The_Other_Hoof
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine().Split(' ');
+            List<string> l = new List<string>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (l.Contains(input[i])) continue;
+                l.Add(input[i]);
+            }
+            Console.WriteLine(4 - l.Count);
+            Console.ReadLine();
         }
     }
 }
