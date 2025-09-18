543cbbf330fcf5a1bb6ceda5d98f5287134afbf0
tuananh <anhntlinh@gmail.com>
2025-09-19 01:33:46 +0700

    complete

--- Pangram/Program.cs
+++ Pangram/Program.cs
@@ -6,6 +6,21 @@ namespace Pangram
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            string s = Console.ReadLine().ToLower();
+            bool isHave = true;
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                if (s.Contains(c.ToString())) continue;
+                isHave = false;
+                break;
+            }
+
+            if (isHave)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            Console.ReadLine();
         }
     }
 }
