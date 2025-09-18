36aae359b123f60472f18a4055bf92ff3444a3d1
tuananh <anhntlinh@gmail.com>
2025-09-18 10:07:03 +0700

    complete

--- Ultra_Fast_Mathematician/Program.cs
+++ Ultra_Fast_Mathematician/Program.cs
@@ -6,6 +6,19 @@ namespace Ultra_Fast_Mathematician
     {
         static void Main(string[] args)
         {
+            string n1 = Console.ReadLine();
+            string n2 = Console.ReadLine();
+            int len = n1.Length;
+            string res = "";
+            for (int i = 0; i < len; i++)
+            {
+                //if (n1[i] != n2[i]) res += '1';
+                //else res += '0';
+                if (n1[i] != n2[i]) Console.Write(1);
+                else Console.Write(0);
+            }
+            // Console.WriteLine(res);
+            Console.ReadLine();
         }
     }
 }
