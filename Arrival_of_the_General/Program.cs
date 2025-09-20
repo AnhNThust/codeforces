d299c5e64122948c78c64f88bed51b134899cc67
tuananh <anhntlinh@gmail.com>
2025-09-20 12:25:34 +0700

    completed but not optimized

--- Arrival_of_the_General/Program.cs
+++ Arrival_of_the_General/Program.cs
@@ -6,6 +6,37 @@ namespace Arrival_of_the_General
     {
         static void Main(string[] args)
         {
+            int n = Int32.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            int imax = 0;
+            int imin = n - 1;
+            int max = Int32.Parse(input[imax]);
+            int min = Int32.Parse(input[imin]);
+
+            for (int i = 1, j = n - 2; i < n; i++)
+            {
+                int ni = Int32.Parse(input[i]);
+                int nj = Int32.Parse(input[j]);
+
+                if (ni > max)
+                {
+                    max = ni;
+                    imax = i;
+                }
+
+                if (nj < min)
+                {
+                    min = nj;
+                    imin = j;
+                }
+                j--;
+            }
+
+            if (imax < imin)
+                Console.WriteLine(n - 1 - imin + imax);
+            else
+                Console.WriteLine(n - 2 - imin + imax);
+            Console.ReadLine();
         }
     }
 }
