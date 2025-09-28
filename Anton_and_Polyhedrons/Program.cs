ccfe7d17e27cc7a8e59cc53c048b983cc5a5077e
tuananh <anhntlinh@gmail.com>
2025-09-28 11:06:45 +0700

    complete Insomnia cure and create project "anton and polyhedrons"

---
+++ Anton_and_Polyhedrons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anton_and_Polyhedrons
+{
+      class Program
+      {
+            static void Main(string[] args)
+            {
+
+            }
+      }
+}
--- Insomnia_cure/Program.cs
+++ Insomnia_cure/Program.cs
@@ -2,10 +2,28 @@
 
 namespace Insomnia_cure
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+      class Program
+      {
+            static void Main(string[] args)
+            {
+                  int[] t = new int[4];
+                  for (int i = 0; i < 4; i++)
+                        t[i] = int.Parse(Console.ReadLine());
+
+                  int d = int.Parse(Console.ReadLine());
+                  int count = 0;
+                  for (int i = 1; i <= d; i++)
+                  {
+                        for (int j = 0; j < 4; j++)
+                        {
+                              if (i % t[j] != 0) continue;
+                              count++;
+                              break;
+                        }
+                  }
+
+                  Console.WriteLine(count);
+                  Console.ReadLine();
+            }
+      }
 }
