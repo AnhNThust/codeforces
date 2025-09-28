02928616406c8d1d63d8dc4ed2ff2b256c124655
tuananh <anhntlinh@gmail.com>
2025-09-28 12:17:20 +0700

    complete

--- Anton_and_Polyhedrons/Program.cs
+++ Anton_and_Polyhedrons/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Anton_and_Polyhedrons
 {
-      class Program
-      {
-            static void Main(string[] args)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < n; i++)
             {
-
+                string s = Console.ReadLine();
+                switch (s)
+                {
+                    case "Tetrahedron":
+                        count += 4;
+                        break;
+                    case "Cube":
+                        count += 6;
+                        break;
+                    case "Octahedron":
+                        count += 8;
+                        break;
+                    case "Dodecahedron":
+                        count += 12;
+                        break;
+                    case "Icosahedron":
+                        count += 20;
+                        break;
+                    default:
+                        break;
+                }
             }
-      }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
 }
