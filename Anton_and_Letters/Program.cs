4c004f3bae57eab3015f5fd5ad755027e1b75ec3
tuananh <anhntlinh@gmail.com>
2025-09-28 14:17:45 +0700

    complete

--- Anton_and_Letters/Program.cs
+++ Anton_and_Letters/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Anton_and_Letters
 {
@@ -6,6 +7,23 @@ namespace Anton_and_Letters
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            Console.WriteLine(DistinctLetters(s));
+            Console.ReadLine();
+        }
+
+        static int DistinctLetters(string s)
+        {
+            int len = s.Length;
+            if (len < 4) return len - 2;
+
+            List<char> list = new List<char>();
+            for (int i = 1; i < len - 1; i++)
+            {
+                if (s[i] < 'a' || s[i] > 'z' || list.Contains(s[i])) continue;
+                list.Add(s[i]);
+            }
+            return list.Count;
         }
     }
 }
