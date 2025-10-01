5a0e0852f618784e1e4ee08e1d3a2cb3dd42ec8c
tuananh <anhntlinh@gmail.com>
2025-10-01 09:33:04 +0700

    complete

--- 268A/Program.cs
+++ 268A/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _268A
 {
@@ -6,6 +7,28 @@ namespace _268A
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list1 = new List<string>();
+            List<string> list2 = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                list1.Add(s[0]);
+                list2.Add(s[1]);
+            }
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j || list1[i] != list2[j]) continue;
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+            Console.ReadLine();
         }
     }
 }
