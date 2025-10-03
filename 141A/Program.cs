439dde7d29302a066321fd3fb8bed1bb8332ac75
tuananh <anhntlinh@gmail.com>
2025-10-03 17:56:40 +0700

    codeforces bi sap, tam thoi chua chay test case duoc

--- 141A/Program.cs
+++ 141A/Program.cs
@@ -6,6 +6,36 @@ namespace _141A
     {
         static void Main(string[] args)
         {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string all = Console.ReadLine();
+            Console.WriteLine(CheckLetters(s1, s2, all));
+            Console.ReadLine();
+        }
+
+        static string CheckLetters(string s1, string s2, string s3)
+        {
+            if (s1.Length + s2.Length != s3.Length) return "NO";
+            string total = s1 + s2;
+            int len = total.Length;
+
+            char head = 'A';
+            while (head <= 'Z')
+            {
+                int count1 = 0;
+                int count2 = 0;
+                for (int i = 0; i < len; i++)
+                {
+                    if (total[i] == head)
+                        count1++;
+                    if (s3[i] == head)
+                        count2++;
+                }
+                if (count1 != count2)
+                    return "NO";
+                head++;
+            }
+            return "YES";
         }
     }
 }
