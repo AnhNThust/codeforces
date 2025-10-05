0ddf0cfc754dc744515364092a56baa08d17efd0
tuananh <anhntlinh@gmail.com>
2025-10-05 11:49:21 +0700

    completed

--- 723A/Program.cs
+++ 723A/Program.cs
@@ -7,18 +7,26 @@ namespace _723A
         static void Main(string[] args)
         {
             string[] s = Console.ReadLine().Split(' ');
+            int[] ns = new int[s.Length];
             for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 2; j++)
-                        {
-                              if (s[i] > s[i + 1])
-                        {
-                              string temp = s[i];
-                              s[i] = s[i+1];
-                              s[i+1] = temp;
-                        }
-                        }
+                  ns[i] = int.Parse(s[i]);
             }
+
+            for (int i = 0; i < 3; i++)
+            {
+                  for (int j = 0; j < 2; j++)
+                  {
+                        if (ns[j] > ns[j+1])
+                        {
+                              int temp = ns[j];
+                              ns[j] = ns[j+1];
+                              ns[j+1] = temp;
+                        }
+                  }
+            }
+            Console.WriteLine(ns[2] - ns[0]);
+            Console.ReadLine();
         }
     }
 }
