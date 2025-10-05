e587b95177b7113268946650e89a249deeb0673d
tuananh <anhntlinh@gmail.com>
2025-10-05 17:24:44 +0700

    create project

--- 1742A/Program.cs
+++ 1742A/Program.cs
@@ -6,6 +6,18 @@ namespace _1742A
       {
             static void Main(string[] args)
             {
+                  int t = int.Parse(Console.ReadLine());
+                  for (int i = 0; i < t; i++)
+                  {
+                        string[] s = Console.ReadLine().Trim().Split(' ');
+                        int a = int.Parse(s[0]);
+                        int b = int.Parse(s[1]);
+                        int c = int.Parse(s[2]);
+                        if (a + b == c || a + c == b || b + c == a)
+                              Console.WriteLine("YES");
+                        else
+                              Console.WriteLine("NO");
+                  }
             }
       }
 }
