cb43efb0e34d40cda1a834a347a9f24b951d4693
tuananh <anhntlinh@gmail.com>
2025-10-05 11:13:05 +0700

    file cannot format

--- 723A/Program.cs
+++ 723A/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace _723A
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] input = Console.ReadLine().Split(' ');
-            int middle = int.Parse(input[0]);
-            for (int i = 1; i < 3; i++)
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < 3; i++)
             {
-                //if (input[i] > )
+                for (int j = 0; j < 2; j++)
+                        {
+                              if (s[i] > s[i + 1])
+                        {
+                              string temp = s[i];
+                              s[i] = s[i+1];
+                              s[i+1] = temp;
+                        }
+                        }
             }
         }
     }
