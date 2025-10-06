baff4088c1588a071e088f0a93d5beb2a906dd78
tuananh <anhntlinh@gmail.com>
2025-10-06 09:29:35 +0700

    completed

--- 427A/Program.cs
+++ 427A/Program.cs
@@ -6,6 +6,31 @@ namespace _427A
       {
             static void Main(string[] args)
             {
+                  int n = int.Parse(Console.ReadLine());
+                  string[] s = Console.ReadLine().Trim().Split(' ');
+                  int[] ns = new int[n];
+                  for (int i = 0; i < n; i++)
+                  {
+                        ns[i] = int.Parse(s[i]);
+                  }
+
+                  int count = 0;
+                  int sum = 0;
+                  for (int i = 0; i < n; i++)
+                  {
+                        if (sum > 0)
+                              sum += ns[i];
+                        else
+                        {
+                              if (ns[i] > 0)
+                                    sum += ns[i];
+                              else
+                                    count++;
+                        }
+                  }
+
+                  Console.WriteLine(count);
+                  Console.ReadLine();
             }
       }
 }
