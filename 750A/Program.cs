437ea459efa6b1d2412ed008060cb81dac4f293e
tuananh <anhntlinh@gmail.com>
2025-10-07 12:03:40 +0700

    completed

--- 750A/Program.cs
+++ 750A/Program.cs
@@ -6,6 +6,20 @@ namespace _750A
     {
         static void Main(string[] args)
         {
+            string[] s = Console.ReadLine().Split(' ');
+            int n = int.Parse(s[0]);
+            int k = int.Parse(s[1]);
+            int limit = 240;
+            int sum = 0;
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += (5 * i);
+                if (sum + k <= limit)
+                    count++;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
         }
     }
 }
