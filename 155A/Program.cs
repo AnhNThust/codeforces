b8cd3c8ed61ec8e0d385f6b6802195c4e720cf8f
tuananh <anhntlinh@gmail.com>
2025-10-07 10:50:02 +0700

    completed

--- 155A/Program.cs
+++ 155A/Program.cs
@@ -6,6 +6,25 @@ namespace _155A
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split(' ');
+            int count = 0;
+            int min = int.Parse(s[0]);
+            int max = min;
+            for (int i = 1; i < n; i++)
+            {
+                int t = int.Parse(s[i]);
+                if (t < min || t > max)
+                {
+                    count++;
+                    if (t < min)
+                        min = t;
+                    else if (t > max)
+                        max = t;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
         }
     }
 }
