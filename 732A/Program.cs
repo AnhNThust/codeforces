3cdff2ec8e3b2a30f9bd3bbf4cf9e7988da496e4
tuananh <anhntlinh@gmail.com>
2025-10-11 10:50:00 +0700

    completed

--- 732A/Program.cs
+++ 732A/Program.cs
@@ -6,6 +6,19 @@ namespace _732A
     {
         static void Main(string[] args)
         {
+            string[] s = Console.ReadLine().Split(' ');
+            int k = int.Parse(s[0]);
+            int r = int.Parse(s[1]);
+            int i = 1;
+            while (true)
+            {
+                int total = k * i;
+                if (total % 10 == 0 || total % 10 == r)
+                    break;
+                i++;
+            }
+            Console.WriteLine(i);
+            Console.ReadLine();
         }
     }
 }
