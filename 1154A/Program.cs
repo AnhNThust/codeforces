739474f320261182c1b4ea92f9826b4cc96e8c87
tuananh <anhntlinh@gmail.com>
2025-10-12 11:52:31 +0700

    completed

--- 1154A/Program.cs
+++ 1154A/Program.cs
@@ -6,6 +6,25 @@ namespace _1154A
     {
         static void Main(string[] args)
         {
+            string[] s = Console.ReadLine().Split();
+            int len = s.Length;
+            int[] n = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                n[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (n[i] < n[i + 1]) continue;
+
+                int temp = n[i];
+                n[i] = n[i + 1];
+                n[i + 1] = temp;
+            }
+
+            Console.WriteLine($"{n[3] - n[2]} {n[3] - n[1]} {n[3] - n[0]}");
+            Console.ReadLine();
         }
     }
 }
