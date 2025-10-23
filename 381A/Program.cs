0235b6d4ce59ac9d4667949ea35ce550c610e8d4
tuananh <anhntlinh@gmail.com>
2025-10-23 12:40:04 +0700

    completed

--- 381A/Program.cs
+++ 381A/Program.cs
@@ -6,6 +6,51 @@ namespace _381A
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split(' ');
+            int[] ns = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                ns[i] = int.Parse(s[i]);
+            }
+
+            int j = 0;
+            int k = n - 1;
+            int sereja = 0;
+            int dima = 0;
+            bool isDima = false;
+            while (j <= k)
+            {
+                if (isDima == false)
+                {
+                    if (ns[j] >= ns[k])
+                    {
+                        sereja += ns[j];
+                        j++;
+                    }
+                    else
+                    {
+                        sereja += ns[k];
+                        k--;
+                    }
+                }
+                else
+                {
+                    if (ns[j] >= ns[k])
+                    {
+                        dima += ns[j];
+                        j++;
+                    }
+                    else
+                    {
+                        dima += ns[k];
+                        k--;
+                    }
+                }
+                isDima = !isDima;
+            }
+
+            Console.WriteLine($"{sereja} {dima}");
         }
     }
 }
