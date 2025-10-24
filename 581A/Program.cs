d5a75cb207b598342e22398abe85c5af44d5b044
tuananh <anhntlinh@gmail.com>
2025-10-24 09:09:32 +0700

    completed

--- 581A/Program.cs
+++ 581A/Program.cs
@@ -6,6 +6,13 @@ namespace _581A
     {
         static void Main(string[] args)
         {
+            string[] s = Console.ReadLine().Split(' ');
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+            Console.Write(a < b ? a : b);
+            int c = Math.Abs(a - b);
+            Console.Write($" {c / 2}");
+            Console.ReadLine();
         }
     }
 }
