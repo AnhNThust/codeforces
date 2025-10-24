ba72f8ea41ce68099628a4091dbec440bced1e10
tuananh <anhntlinh@gmail.com>
2025-10-24 09:46:27 +0700

    completed

--- 1669A/Program.cs
+++ 1669A/Program.cs
@@ -6,6 +6,19 @@ namespace _1669A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int rate = int.Parse(Console.ReadLine());
+                Console.WriteLine(Rating(rate));
+            }
+        }
+
+        static string Rating(int rate)
+        {
+            return rate <= 1399 ? "Division 4" :
+                rate <= 1599 ? "Division 3" :
+                rate <= 1899 ? "Division 2" : "Division 1";
         }
     }
 }
