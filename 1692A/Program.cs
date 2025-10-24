f452312fd1d3856d1b199acf67a98f3596ebd023
tuananh <anhntlinh@gmail.com>
2025-10-24 12:06:02 +0700

    completed

--- 1692A/Program.cs
+++ 1692A/Program.cs
@@ -6,6 +6,20 @@ namespace _1692A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                int a = int.Parse(s[0]);
+                int count = 0;
+                for (int j = 1 ; j < 4; j++)
+                {
+                    int n = int.Parse(s[j]);
+                    if (n > a)
+                        count++;
+                }
+                Console.WriteLine(count);
+            }
         }
     }
 }
