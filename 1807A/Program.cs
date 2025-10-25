cb48d06dd43265eea72ce69b94157ef63f79d741
tuananh <anhntlinh@gmail.com>
2025-10-25 11:20:42 +0700

    completed

--- 1807A/Program.cs
+++ 1807A/Program.cs
@@ -6,6 +6,18 @@ namespace _1807A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+                int c = int.Parse(s[2]);
+                if (a + b == c)
+                    Console.WriteLine('+');
+                else
+                    Console.WriteLine('-');
+            }
         }
     }
 }
