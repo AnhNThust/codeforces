471d8d9fa5e208363cdb11d7d342854213de8a28
tuananh <anhntlinh@gmail.com>
2025-10-25 10:16:16 +0700

    completed

--- 1676A/Program.cs
+++ 1676A/Program.cs
@@ -6,6 +6,24 @@ namespace _1676A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string s = Console.ReadLine();
+                int sum1 = 0;
+                int sum2 = 0;
+                for (int j = 0; j < 6; j++)
+                {
+                    if (j < 3)
+                        sum1 += (s[j] - '0');
+                    else
+                        sum2 += (s[j] - '0');
+                }
+                if (sum1 == sum2)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
         }
     }
 }
