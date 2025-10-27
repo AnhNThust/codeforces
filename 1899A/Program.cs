354cd3c04f3a4a3326faa6b864b4c879b46f50f4
tuananh <anhntlinh@gmail.com>
2025-10-27 12:28:30 +0700

    completed

--- 1899A/Program.cs
+++ 1899A/Program.cs
@@ -6,6 +6,15 @@ namespace _1899A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n % 3 == 0)
+                    Console.WriteLine("Second");
+                else
+                    Console.WriteLine("First");
+            }
         }
     }
 }
