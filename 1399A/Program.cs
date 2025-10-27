ad43545c24723846f59de3cc7e17101cf461a2d7
tuananh <anhntlinh@gmail.com>
2025-10-27 09:13:20 +0700

    Nothing

--- 1399A/Program.cs
+++ 1399A/Program.cs
@@ -6,6 +6,36 @@ namespace _1399A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                string[] s = Console.ReadLine().Split(' ');
+                int[] ns = new int[s.Length];
+                
+                for (int j = 0; j < s.Length; j++)
+                {
+                    ns[j] = int.Parse(s[j]);
+                }
+                Console.WriteLine(RemoveSmallest(ns));
+            }
+        }
+
+        static string RemoveSmallest(int[] arr)
+        {
+            if (arr.Length <= 1) return "YES";
+
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (Math.Abs(arr[i] - arr[i + 1]) <= 1)
+                {
+                    continue;
+                }
+                else
+                    return "NO";
+            }
+            return "YES";
         }
     }
 }
