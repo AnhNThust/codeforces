c3fdbd110558acaa40eade16408e9da02be32aa1
tuananh <anhntlinh@gmail.com>
2025-10-29 11:50:21 +0700

    completed

--- 1760A/Program.cs
+++ 1760A/Program.cs
@@ -6,6 +6,17 @@ namespace _1760A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                int[] ns = new int[3];
+                ns[0] = int.Parse(s[0]);
+                ns[1] = int.Parse(s[1]);
+                ns[2] = int.Parse(s[2]);
+                Array.Sort(ns);
+                Console.WriteLine(ns[1]);
+            }
         }
     }
 }
