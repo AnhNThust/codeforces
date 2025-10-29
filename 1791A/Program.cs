0c40d16aaf7105e3ac65239ee1ebdae71414e7e5
tuananh <anhntlinh@gmail.com>
2025-10-29 10:15:25 +0700

    completed

--- 1791A/Program.cs
+++ 1791A/Program.cs
@@ -6,6 +6,16 @@ namespace _1791A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            string s = "codeforces";
+            for (int i = 0; i < t; i++)
+            {
+                string c = Console.ReadLine();
+                if (s.IndexOf(c) > -1)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
         }
     }
 }
