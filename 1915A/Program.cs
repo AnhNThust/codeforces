74bcac46cd7ada1409eef7aed7a5fbfdb5dd8a4f
tuananh <anhntlinh@gmail.com>
2025-10-29 12:28:53 +0700

    completed

--- 1915A/Program.cs
+++ 1915A/Program.cs
@@ -6,6 +6,22 @@ namespace _1915A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                int[] ns = new int[3];
+                ns[0] = int.Parse(s[0]);
+                ns[1] = int.Parse(s[1]);
+                ns[2] = int.Parse(s[2]);
+                Console.WriteLine(FindNumber(ns));
+            }
+        }
+
+        static int FindNumber(int[] ns)
+        {
+            return ns[0] == ns[1] ? ns[2] :
+                    ns[0] == ns[2] ? ns[1] : ns[0];
         }
     }
 }
