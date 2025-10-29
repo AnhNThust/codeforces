2a85b885e448cda4b8cde57e609668a2c2541f16
tuananh <anhntlinh@gmail.com>
2025-10-29 10:07:45 +0700

    completed

--- 1512A/Program.cs
+++ 1512A/Program.cs
@@ -6,6 +6,32 @@ namespace _1512A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                string[] s = Console.ReadLine().Split(' ');
+                int spy = 0;
+                for (int j = 0; j < n - 2; j++)
+                {
+                    if (s[j] != s[j + 1] && s[j + 1] != s[j + 2])
+                    {
+                        spy = j + 1;
+                        break;
+                    }
+                    else if(s[j] != s[j + 1] && s[j + 1] == s[j + 2])
+                    {
+                        spy = j;
+                        break;
+                    }
+                    else if (s[j] == s[j + 1] && s[j + 1] != s[j + 2])
+                    {
+                        spy = j + 2;
+                        break;
+                    }
+                }
+                Console.WriteLine(spy + 1);
+            }
         }
     }
 }
