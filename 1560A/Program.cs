a6d28d9ecaf3e01c5259e229e845b0a6149a6ce4
tuananh <anhntlinh@gmail.com>
2025-10-30 11:02:03 +0700

    completed

--- 1560A/Program.cs
+++ 1560A/Program.cs
@@ -6,7 +6,20 @@ namespace _1560A
     {
         static void Main(string[] args)
         {
-
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int count = 0;
+                int num = 0;
+                int k = int.Parse(Console.ReadLine());
+                while (count < k)
+                {
+                    num++;
+                    if (num % 3 != 0 && num % 10 != 3)
+                        count++;
+                }
+                Console.WriteLine(num);
+            }
         }
     }
 }
