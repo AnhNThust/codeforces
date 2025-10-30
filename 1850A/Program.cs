5b4afd61578179dca91d86850f5e6710cebc7865
tuananh <anhntlinh@gmail.com>
2025-10-30 12:26:56 +0700

    completed

--- 1850A/Program.cs
+++ 1850A/Program.cs
@@ -6,6 +6,18 @@ namespace _1850A
     {
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+                int c = int.Parse(s[2]);
+                if (a + b >= 10 || a + c >= 10 || b + c >= 10)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
         }
     }
 }
